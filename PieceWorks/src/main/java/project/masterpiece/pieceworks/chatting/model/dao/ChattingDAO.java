a0c86bf210951d8875613aac32c3a8717f958c3a
hyeon-woo1 <85299367+hyeon-woo1@users.noreply.github.com>
2021-12-30 20:11:37 +0900

    채팅창 새창으로 만들었습니다

---
+++ PieceWorks/src/main/java/project/masterpiece/pieceworks/chatting/model/dao/ChattingDAO.java
@@ -0,0 +1,26 @@
+package project.masterpiece.pieceworks.chatting.model.dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import project.masterpiece.pieceworks.chatting.model.vo.ChattingMessage;
+
+@Repository("cDAO")
+public class ChattingDAO {
+
+	
+
+	public ArrayList<ChattingMessage> selectChattingMessageList(SqlSessionTemplate sqlSession,
+			ChattingMessage c) {
+		
+				
+		return (ArrayList)sqlSession.selectList("chattingMapper.selectChattingMessageList",c);
+	}
+
+	
+
+}
---
+++ PieceWorks/src/main/java/project/masterpiece/pieceworks/chatting/model/service/ChattingServiceImpl.java
@@ -0,0 +1,31 @@
+package project.masterpiece.pieceworks.chatting.model.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+import project.masterpiece.pieceworks.chatting.model.dao.ChattingDAO;
+import project.masterpiece.pieceworks.chatting.model.vo.ChattingMessage;
+
+@Service("cService")
+public class ChattingServiceImpl implements ChattingService{
+
+	@Autowired
+	private ChattingDAO cDAO;
+		
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+
+
+	@Override
+	public ArrayList<ChattingMessage> selectChattingMessageList(ChattingMessage c) {
+		return cDAO.selectChattingMessageList(sqlSession,c);
+	}
+
+}
